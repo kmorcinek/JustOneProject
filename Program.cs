6cb64a7750a7b855d62ef20640aacd8928a26500
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2013-07-01 11:16:03 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
