12caff698e9a16ce89f54dcaffed46a94ebb922d
Krzysztof Morcinek <krzysztof.morcinek@smtsoftware.com>
2015-10-31 11:39:29 +0100

    GitPatchGenerator

---
+++ VariousStuff/GitPatchesGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+
+namespace JustOneProject.VariousStuff
+{
+    public class GitPatchesGenerator
+    {
+        public void GeneratePatches(string directoryPath)
+        {
+            var inputFile = Path.Combine(directoryPath + "my_hashes.txt");
+            var lines = File.ReadAllLines(inputFile);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var hash in lines)
+            {
+                sb.AppendLine("git format-patch -1 " + hash);
+            }
+
+            var outputFile = Path.Combine(directoryPath, "patches.cmd");
+            File.WriteAllText(outputFile, sb.ToString());
+        }
+    }
+}
