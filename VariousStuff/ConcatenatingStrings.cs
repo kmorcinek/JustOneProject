e06ff6f9cbca8da2e79cb2aea79242a4af9bc4a0
Krzysztof Morcinek <krzysztof.morcinek@smtsoftware.com>
2015-11-09 22:08:06 +0100

    ConcatenatingStrings

---
+++ VariousStuff/ConcatenatingStrings.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace JustOneProject.VariousStuff
+{
+    public class ConcatenatingStrings
+    {
+        public void Foo()
+        {
+            var names = new[] { "john", "james", "robin" }; // and many, many more
+
+            // This is how I was doing it by always default
+            var sb = new StringBuilder();
+            foreach (var name in names)
+            {
+                sb.Append(name);
+            }
+
+            var result = sb.ToString();
+
+            // This is better cause of readability
+            var result2 = string.Concat(names);
+        }
+    }
+}
