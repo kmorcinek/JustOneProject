ebe5bd1c8e642ab39b32b63800d352d8edb00dad
Krzysztof Morcinek <krzysztof.morcinek@smtsoftware.com>
2015-11-16 21:51:43 +0100

    EnumHelper class

---
+++ Extensions/EnumHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JustOneProject.Extensions
+{
+    public class EnumHelper
+    {
+        public static TEnum GetEnumFromString<TEnum>(string value) where TEnum : struct, IComparable
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException(string.Format("{0} must be an enumerated type", typeof(TEnum).FullName));
+            }
+
+            return (TEnum)Enum.Parse(typeof(TEnum), value, true);
+        }
+    }
+}
