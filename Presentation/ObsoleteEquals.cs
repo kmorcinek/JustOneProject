5a587e686a98781e56c05968cc0a2053e0559af5
Krzysztof Morcinek <krzysztof.morcinek@smtsoftware.com>
2015-11-19 04:50:05 +0100

    Presentation, some code

---
+++ Presentation/ObsoleteEquals.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JustOneProject.Presentation
+{
+    public class ObsoleteEquals
+    {
+        public void Foo()
+        {
+            const string user = "John";
+            var input = Console.ReadLine();
+
+            if (user.Equals(input))
+            {
+                Console.WriteLine("Entered correct user");
+            }
+
+            if (user.Equals(input))
+            {
+                Console.WriteLine("Entered correct user");
+            }
+
+            if (user.Equals(input))
+            {
+                Console.WriteLine("Entered correct user");
+            }
+
+        }
+    }
+}
