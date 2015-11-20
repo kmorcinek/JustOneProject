bd3b16a3bd332a099399be9b56c2b918b3d11596
Krzysztof Morcinek <krzysztof.morcinek@smtsoftware.com>
2015-11-20 16:04:12 +0100

    After presentation

---
+++ Presentation/Side.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JustOneProject.Presentation
+{
+    public class Side
+    {
+        void Foo()
+        {
+            if (0 > 5)
+            {
+                Console.WriteLine("hello PWR");
+            }
+
+            var names = new List<string>();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            var firstName = names.FirstOrDefault();
+
+            try
+            {
+                Console.WriteLine(firstName.ToLower());
+                Console.WriteLine(firstName.ToLower());
+                Console.WriteLine(firstName.ToLower());
+                Console.WriteLine(firstName.ToLower());
+                Console.WriteLine(firstName.ToLower());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- VariousStuff/GitPatchesGenerator.cs
+++ VariousStuff/GitPatchesGenerator.cs
@@ -7,18 +7,24 @@ namespace JustOneProject.VariousStuff
     {
         public void GeneratePatches(string directoryPath)
         {
-            var inputFile = Path.Combine(directoryPath + "my_hashes.txt");
+            var pathHashesFile = directoryPath + "my_hashes.txt";
+            var inputFile = Path.Combine(pathHashesFile);
             var lines = File.ReadAllLines(inputFile);
 
             StringBuilder sb = new StringBuilder();
 
+            AppendLInes(lines, sb);
+
+            var outputFile = Path.Combine(directoryPath, "patches.cmd");
+            File.WriteAllText(outputFile, sb.ToString());
+        }
+
+        private static void AppendLInes(string[] lines, StringBuilder sb)
+        {
             foreach (var hash in lines)
             {
                 sb.AppendLine("git format-patch -1 " + hash);
             }
-
-            var outputFile = Path.Combine(directoryPath, "patches.cmd");
-            File.WriteAllText(outputFile, sb.ToString());
         }
     }
 }
