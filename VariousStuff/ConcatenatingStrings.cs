ce472a60ef7a1de9439afa830132d6c857f2560f
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2015-12-11 17:08:41 +0100

    Create ServiceA

--- VariousStuff/ConcatenatingStrings.cs
+++ VariousStuff/ConcatenatingStrings.cs
@@ -14,7 +14,7 @@ namespace JustOneProject.VariousStuff
             {
                 sb.Append(name);
             }
-
+            sd
             var result = sb.ToString();
 
             // This is better cause of readability
