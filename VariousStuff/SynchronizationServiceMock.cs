6ed6bd7b8abfc2811e810bead2c18a817e76b652
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2015-12-11 16:37:12 +0100

    SynchronizationServiceMock

---
+++ VariousStuff/SynchronizationServiceMock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace JustOneProject.VariousStuff
+{
+    public class SynchronizationServiceMock
+    {
+        public IEnumerable<string> GetItemsFromFullSynchronization()
+        {
+            // Check current items
+            // Download new and changed items
+            // Make some diffs on data
+            // etc
+            return new[] {"ala", "ma", "kota"};
+        }
+    }
+}
