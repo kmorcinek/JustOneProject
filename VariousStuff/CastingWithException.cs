4d93f85201f22286b06a0bc0e5bc80e8dfa4acf0
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2015-12-14 14:26:41 +0100

    CastingWithException example

---
+++ VariousStuff/CastingWithException.cs
@@ -0,0 +1,13 @@
+﻿namespace JustOneProject.VariousStuff
+{
+    public class CastingWithException
+    {
+        public object Value { get; set; } // Default value is null
+
+        public static void CheckIfCanCast()
+        {
+            var castingWithException = new CastingWithException();
+            bool isGood = (bool) castingWithException.Value;
+        }
+    }
+}
