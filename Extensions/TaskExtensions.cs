957168d6d95c847785d92c823ae3e377b937badd
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-01-27 16:03:35 +0100

    TaskExtensions

---
+++ Extensions/TaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace JustOneProject.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static ConfiguredTaskAwaitable<T> ConfigureAwaitWithoutReturnToContext<T>(this Task<T> task)
+        {
+            return task.ConfigureAwait(continueOnCapturedContext: false);
+        }
+
+        public static ConfiguredTaskAwaitable ConfigureAwaitWithoutReturnToContext(this Task task)
+        {
+            return task.ConfigureAwait(continueOnCapturedContext: false);
+        }
+    }
+}
