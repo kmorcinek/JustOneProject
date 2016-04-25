7254c1597c8ecdb436b55d94a153741cc8d3d480
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-04-25 15:48:18 +0200

    FileConcatenator

---
+++ VariousStuff/FileConcatenator.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+
+namespace JustOneProject.VariousStuff
+{
+    public class FileConcatenator
+    {
+        public void Concat(string path, string pattern)
+        {
+            var enumerateFiles = Directory.EnumerateFiles(path, pattern);
+
+            var sb = new StringBuilder();
+            foreach (var file in enumerateFiles)
+            {
+                var readAllText = File.ReadAllText(file);
+                sb.AppendLine(readAllText);
+            }
+
+            File.WriteAllText("out.sql", sb.ToString());
+        }
+    }
+}
