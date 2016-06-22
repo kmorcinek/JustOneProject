d6b96937d76aed8b7cda11b112770d763da63b9c
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-06-22 10:43:59 +0200

    Add method Path.Helpers.EnsureDirectoryExists()

---
+++ Helpers/PathHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace JustOneProject.Helpers
+{
+    public static class PathHelper
+    {
+        public static void EnsureDirectoryExists(string filePath)
+        {
+            var directoryPath = Path.GetDirectoryName(filePath);
+            Directory.CreateDirectory(directoryPath);
+        }
+    }
+}
