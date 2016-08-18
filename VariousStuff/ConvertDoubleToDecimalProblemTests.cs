0f387ab7b5530d2effa54a07c2d555521e6ee2cd
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-08-18 16:36:59 +0200

    Create ConvertDoubleToDecimalProblemTests

---
+++ VariousStuff/ConvertDoubleToDecimalProblemTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace JustOneProject.VariousStuff
+{
+    public class ConvertDoubleToDecimalProblemTests
+    {
+        [Theory]
+        [InlineData(1.3)]
+        [InlineData(double.MinValue)]
+        [InlineData(double.MaxValue)]
+        public void Foo(double doubleValue)
+        {
+            ConvertToDecimalAndBack(doubleValue);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            double value = 0.000001d;
+            value *= 10;
+//            Console.WriteLine(value); // 9.9999999999999991E-06
+            ConvertToDecimalAndBack(value);
+        }
+
+        private static void ConvertToDecimalAndBack(double doubleValue)
+        {
+            decimal decimalValue = (decimal)doubleValue;
+
+            double doubleResult = (double)decimalValue;
+
+            Assert.Equal(doubleValue, doubleResult);
+        }
+    }
+}
