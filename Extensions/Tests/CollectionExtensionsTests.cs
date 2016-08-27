36a932c1b62ff0b4a380554106b40acd834850c3
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-08-27 21:06:16 +0200

    Add CollectionExtensions.RemoveAll(...) with tests

---
+++ Extensions/Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace JustOneProject.Extensions.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        private readonly int[] _numbers = { 1, 3, 5 };
+        private readonly Func<int, bool> _predicate = x => x > 2;
+
+        [Fact]
+        public void TestList()
+        {
+            ICollection<int> list = new List<int>(_numbers);
+
+            list.RemoveAll(_predicate);
+
+            Assert.Equal(1, list.Count);
+            Assert.Equal(1, list.First());
+        }
+
+        [Fact]
+        public void TestNotList()
+        {
+            ICollection<int> linkedList = new LinkedList<int>(_numbers);
+
+            linkedList.RemoveAll(_predicate);
+
+            Assert.Equal(1, linkedList.Count);
+            Assert.Equal(1, linkedList.First());
+        }
+    }
+}
