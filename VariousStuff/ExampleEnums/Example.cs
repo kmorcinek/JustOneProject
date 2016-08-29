1cfc72497279c5c57637550045c1ee480c6a7dee
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-08-29 23:39:14 +0200

    How to implement Enums. CreateMissingEnumEx()

---
+++ VariousStuff/ExampleEnums/Example.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JustOneProject.VariousStuff.ExampleEnums
+{
+    public class Example
+    {
+        public void Execute(DocumentStatus documentStatus)
+        {
+            try
+            {
+                switch (documentStatus)
+                {
+                    case DocumentStatus.Draft:
+                        // Do sth
+                        break;
+                    case DocumentStatus.Released:
+                        // Do sth
+                        break;
+                    default:
+                        throw Guards.CreateMissingEnumException(nameof(documentStatus), documentStatus);
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+
+                throw;
+            }
+        }
+
+        WorseDocumentStatus AssignInvalidValue()
+        {
+            return (WorseDocumentStatus) 111;
+        }
+    }
+}
