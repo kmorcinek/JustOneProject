b2965b7fc23d5e98fa2421613be344d1acba333f
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-08-29 23:42:46 +0200

    Move MemoryConsumptionTests to separate class

--- Program.cs
+++ Program.cs
@@ -1,76 +1,12 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using JustOneProject.VariousStuff;
 
 namespace JustOneProject
 {
     public class Program
     {
-        private static MemoryGuard _memoryGuard = new MemoryGuard();
-
         [STAThread]
         private static void Main()
         {
-        }
-
-        private static void TestMemoryConsumption()
-        {
-            const int length = 64 * 1000 * 1000;
-            var array = new byte[length];
-            array[9999] = 33;
-            var arrayForAdding = new byte[15 * 1000 * 1000];
-            arrayForAdding[9999] = 33;
-
-            for (int i = 0; i < 8; i++)
-            {
-                array = Merge(array, arrayForAdding);
-            }
-
-            arrayForAdding = new byte[15 * 1000 * 1000];
-            
-            for (int i = 0; i < 8 * 2; i++)
-            {
-                array = Merge(array, arrayForAdding);
-            }
-
-        }
-
-        private static byte[] Merge(byte[] data, byte[] bytesToAppend)
-        {
-            if (data == null)
-            {
-                return bytesToAppend;
-            }
-
-            if (bytesToAppend == null)
-            {
-                return data;
-            }
-
-            var newArrayLength = data.Length + bytesToAppend.Length;
-            var message = "Merge: newArrayLength = " + newArrayLength / (1000 * 1000);
-            Debug.WriteLine(message);
-
-            PrintMemoryUsedByGC();
-            _memoryGuard.UpdateArrayLengthAndCallGcWhenAppropriate(newArrayLength);
-
-            byte[] result = new byte[newArrayLength];
-            Array.Copy(data, 0, result, 0, data.Length);
-            Array.Copy(bytesToAppend, 0, result, data.Length, bytesToAppend.Length);
-            Wait();
-            return result;
-        }
-
-        private static void Wait()
-        {
-            Task.Delay(TimeSpan.FromSeconds(1)).Wait();
-        }
-
-        private static void PrintMemoryUsedByGC()
-        {
-            long totalMemory = GC.GetTotalMemory(false) / 1024;
-            Debug.WriteLine("Total memory in KB: " + totalMemory);
         }
     }
 }
--- VariousStuff/MemoryConsumption/MemoryGuard.cs
+++ VariousStuff/MemoryConsumption/MemoryGuard.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace JustOneProject.VariousStuff
+namespace JustOneProject.VariousStuff.MemoryConsumption
 {
     public class MemoryGuard
     {
