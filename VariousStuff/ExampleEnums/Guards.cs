e41c192aa5cfbb38a90d68a05ea8fa4f0a3ad9dc
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-09-05 18:23:58 +0200

    Add overload fro CreateMissingEnumException for nullable enum types

--- VariousStuff/ExampleEnums/Guards.cs
+++ VariousStuff/ExampleEnums/Guards.cs
@@ -8,7 +8,14 @@ namespace JustOneProject.VariousStuff.ExampleEnums
             where T : struct
         {
             return new ArgumentOutOfRangeException(paramName, value,
-                $"Enum '{value.GetType().Name}' has invalid value '{value}'");
+                $"Enum '{typeof(T).Name}' has invalid value '{value}'");
+        }
+
+        public static ArgumentOutOfRangeException CreateMissingEnumException<T>(string paramName, T? value)
+            where T : struct
+        {
+            return new ArgumentOutOfRangeException(paramName, value,
+                $"Nullable enum '{typeof(T).Name}' has invalid value '{value}'");
         }
     }
 }
