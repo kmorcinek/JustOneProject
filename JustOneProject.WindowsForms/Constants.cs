e284daa004a57531b851a872e77b5c4473948b41
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-09-09 12:58:45 +0200

    Create utility method Messages.ShowError()

---
+++ JustOneProject.WindowsForms/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace JustOneProject.WindowsForms
+{
+    /// <summary>
+    /// Your application constants used to not repeat strings all over and over again
+    /// </summary>
+    public class Constants
+    {
+        public const string WindowsClientTitle = "YourAppName";
+    }
+}
---
+++ JustOneProject.WindowsForms/Utils/Messages.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace JustOneProject.WindowsForms.Utils
+{
+    public static class Messages
+    {
+        public static void ShowError(string message)
+        {
+            MessageBox.Show(message, Constants.WindowsClientTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
