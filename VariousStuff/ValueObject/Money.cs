df8bdeaecaf741aceaa9e7901b43d6e671ec39c4
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2016-09-29 21:48:22 +0200

    Add Money Value Object with Serialization test

---
+++ VariousStuff/ValueObject/Money.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace JustOneProject.VariousStuff.ValueObject
+{
+    /// <summary>
+    /// Example of Value Object, showing serialization problems.
+    /// Implementation is very limited so it's not good general example
+    /// </summary>
+    public class Money
+    {
+        public static Money Create(decimal value, string currency)
+        {
+            // some additional checks
+            return new Money(value, currency);
+        }
+
+        [JsonConstructor]
+        Money(decimal value, string currency)
+        {
+            Value = value;
+            Currency = currency;
+        }
+
+        public decimal Value { get; }
+        public string Currency { get; }
+    }
+}
---
+++ VariousStuff/ValueObject/MoneyTests.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace JustOneProject.VariousStuff.ValueObject
+{
+    public class MoneyTests
+    {
+        [Fact]
+        public void SerializationTest()
+        {
+            var money = Money.Create(1, "USD");
+            
+            string json = JsonConvert.SerializeObject(money);
+            Money result = JsonConvert.DeserializeObject<Money>(json);
+            
+            Assert.Equal(money.Currency, result.Currency);
+        }
+    }
+}
