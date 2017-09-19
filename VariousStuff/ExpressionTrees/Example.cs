63ee65aa190f71cb8dcc381ac44409ccf144890b
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-09-19 22:20:25 +0200

    Wip ExpressionTrees

---
+++ VariousStuff/ExpressionTrees/Example.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace JustOneProject.VariousStuff.ExpressionTrees
+{
+    public static class Example
+    {
+        public static IQueryable<T> OrderByPropertyOrField<T>(
+            this IQueryable<T> queryable,
+            string propertyOrFieldName,
+            bool ascending
+        )
+        {
+            var elementType = typeof(T);
+            var parameter = Expression.Parameter(elementType);
+            var prop = Expression.PropertyOrField(parameter, propertyOrFieldName);
+            var selector = Expression.Lambda(prop, parameter);
+
+            throw new NotImplementedException();
+        }
+    }
+}
