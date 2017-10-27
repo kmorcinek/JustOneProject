8af6ecfc726d1c122aebf1a6598edc4c69ee37e4
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-10-27 13:28:34 +0200

    -- Test file

---
+++ ConventionTests/Tests/TestFiles/ButtLapCore.cs
@@ -0,0 +1,12 @@
+﻿namespace ABB.CDS.Domain.Core.Stack.Model
+{
+    using SharedKernel;
+
+    public class ButtLapCore : Core<LimbWithAirGap, Yoke>
+    {
+        public ButtLapCore(LimbWithAirGap[] columns, Yoke[] yokes)
+            : base(columns, yokes)
+        {
+        }
+    }
+}
