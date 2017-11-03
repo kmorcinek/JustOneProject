320aeb87b16d9032b9dec040ef62f3a2380ec0f5
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-11-03 11:44:45 +0100

    VariousStuff.AutoFixture

---
+++ VariousStuff/AutoFixture/NewAutofixtureInstanceTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace JustOneProject.VariousStuff.AutoFixture
+{
+    public class NewAutofixtureInstanceTest
+    {
+        [Fact]
+        public void Each_instance_generates_new_ints()
+        {
+            var a = new Fixture().Create<byte>();
+            var b = new Fixture().Create<byte>();
+
+            // It will sometimes fail with 1/256 probability
+            Assert.Equal(a, b);
+        }
+
+        [Fact]
+        public void Same_instance_will_not_repeat_values()
+        {
+            var hashSet = new Dictionary<byte, object>();
+
+            var fixture = new Fixture();
+
+            for (int i = 0; i < byte.MaxValue - 1; i++)
+            {
+                var value = fixture.Create<byte>();
+
+                hashSet.Add(value, null);
+            }
+
+            // In the same Fixture generated number will not be repeated
+        }
+    }
+}
