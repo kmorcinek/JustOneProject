cc22e830ef29b5af833d9b92559942e60dcdb99c
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-11-18 19:05:35 +0100

    RoslynExample wip

---
+++ RoslynExample/EpisodeThree.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace RoslynExample
+{
+    class EpisodeThree
+    {
+        static void Main()
+        {
+            var myWalker = new MyWalker();
+
+            var tree = CSharpSyntaxTree.ParseText(@"
+class C
+{
+    void Method()
+    {
+        // this is method
+    }
+}
+");
+            var root = tree.GetRoot();
+
+            myWalker.Visit(root);
+
+            var result = myWalker.sb.ToString();
+        }
+    }
+
+    public class MyWalker : CSharpSyntaxWalker
+    {
+        public MyWalker()
+            : base(SyntaxWalkerDepth.Token)
+        {
+        }
+
+        public StringBuilder sb = new StringBuilder();
+
+        public override void VisitToken(SyntaxToken token)
+        {
+            sb.Append(token.ToString());
+        }
+    }
+}
---
+++ RoslynExample/EpisodeTwo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynExample
+{
+    class EpisodeTwo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var tree = CSharpSyntaxTree.ParseText(@"
+class C
+{
+    void Method()
+    {
+        // this is method
+    }
+}
+");
+
+            //var diagnostics = tree.GetDiagnostics().Where(x => x.Severity == DiagnosticSeverity.Error).First();
+
+            var root = tree.GetRoot();
+            var method = root.DescendantNodes().OfType<MethodDeclarationSyntax>().First();
+
+            var returnTYpe = SyntaxFactory.ParseTypeName("string");
+
+            var newMethod = method.WithReturnType(returnTYpe);
+        }
+    }
+}
